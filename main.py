d1b6f2b08079182a6f2dd274a2b004696eb00976
Serban2001 <serbanduta2001@gmail.com>
2023-11-08 09:27:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+try:
+    f = open('Accounts.txt', 'r')
+    f.close()
+except FileNotFoundError:
+    f = open('Accounts.txt', 'w')
+    f.close()
+
+import menu1
+import os
+
+os.system('clear')
+menu1.menu1()
